7f5f0e3f7ec996a1d359644bb37a41727ef68d89
nastraw <nathanielwaayne@gmail.com>
2021-04-30 23:28:45 -0400

    Update init.sql to create WhatTheBug database with SchemaVersion table with initial record [WTB-1]

--- Setup/Database/init.sql
+++ Setup/Database/init.sql
@@ -1,4 +1,33 @@
 ﻿IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = 'WhatTheBug')
 BEGIN
-CREATE DATABASE [DataBase]
+CREATE DATABASE [WhatTheBug]
 END
+GO
+
+USE [WhatTheBug]
+GO
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='SchemaVersion')
+BEGIN
+  CREATE TABLE SchemaVersion (
+    [Version] INT PRIMARY KEY,
+    [Description] VARCHAR(100),
+    Migrated DATETIME DEFAULT CURRENT_TIMESTAMP
+  )
+END
+
+IF EXISTS (SELECT *FROM sysobjects WHERE name='SchemaVersion_Migrate')
+BEGIN
+  DROP PROCEDURE SchemaVersion_Migrate
+END
+GO
+
+CREATE PROCEDURE SchemaVersion_Migrate @Version int, @Description VARCHAR(100)
+  AS
+  BEGIN
+    SET NOCOUNT ON
+    INSERT INTO SchemaVersion ([Version], [Description]) VALUES (@Version, @Description)
+  END
+GO
+
+EXEC SchemaVersion_Migrate @Version = 1, @Description = 'Initial schema'
