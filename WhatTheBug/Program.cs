5c3e39810479e8c3ea76d123ed2c10c1fb34fcb8
nastraw <nathanielwaayne@gmail.com>
2021-04-30 20:56:07 -0400

    Project init, .idea folder to git ignore

---
+++ WhatTheBug/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WhatTheBug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
